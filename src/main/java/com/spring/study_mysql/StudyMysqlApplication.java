aa7adf812d49eee99d21ccb01666dd330a7e9fec
kaebit <kbaek1202@gmail.com>
2019-11-15 07:57:19 +0900

    Initial commit

---
+++ src/main/java/com/spring/study_mysql/StudyMysqlApplication.java
@@ -0,0 +1,11 @@
+package com.spring.study_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMysqlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudyMysqlApplication.class, args);
+    }
+}
